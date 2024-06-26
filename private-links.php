f8b57afa1bda89d602a3929e19cb90c67e40925b
Matt Jones <me@mattjones.tech>
2024-06-26 19:18:18 -0400

    bump version number

--- private-links.php
+++ private-links.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Private Links
  * Description: Generate one-time-use links that expire after 24 hours. 
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Matt Jones
  */
 
@@ -58,9 +58,9 @@ function pl_generate_user_token($user_id) {
 }
 
 //Send email with private link
-function pl_send_private_link_email($user_email, $user_id, $email_subject) {
+function pl_send_private_link_email($user_email, $user_id, $email_subject, $page_slug) {
   $token = pl_generate_user_token($user_id);
-  $private_link = home_url('/special-page/?access_token=' . $token);
+  $private_link = home_url($page_slug . '?access_token=' . $token);
 
   $message = 'Here is your private link: ' . $private_link;
 
@@ -137,6 +137,10 @@ function pl_admin_page() {
 <th scope="row">Email Subject</th>
 <td><input type="text" name="email_subject" required /></td>
 </tr>
+<tr>
+<th>Private Page Slug (including leading and trailing slashes)</th>
+<td><input type="text" name="page_slug" required /></td>
+</tr>
 </table>
 <input type="submit" name="submit" class="button-primary" value="Send Email" />
 </form>
@@ -147,9 +151,10 @@ function pl_admin_page() {
     $user_email = sanitize_email($_POST['user_email']);
     $user_id = intval($_POST['user_id']);
     $email_subject = sanitize_text_field($_POST['email_subject']);
+    $page_slug = sanitize_text_field($_POST['page_slug']);
 
     if ($user_email && $user_id) {
-      pl_send_private_link_email($user_email, $user_id, $email_subject);
+      pl_send_private_link_email($user_email, $user_id, $email_subject, $page_slug);
       echo '<div class="notice notice-success is-dismissible"><p>Email sent successfully!</p></div>';
     } else {
       echo '<div class="notice notice-error is-dismissible"><p>Invalid input. Please try again.</p></div>';
