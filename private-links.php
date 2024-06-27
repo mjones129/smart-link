36f8f5df164f27eb2c44b72cc02950c96dd5225b
Matt Jones <me@mattjones.tech>
2024-06-26 20:44:32 -0400

    bring in PHPMailer setup

--- private-links.php
+++ private-links.php
@@ -10,18 +10,6 @@ if (!defined('ABSPATH')) {
   exit; //Exit if accessed directly
 }
 
-//begin PHPmailer setup
-
-//Import the PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
-
-require './vendor/autoload.php';
-
 // create db table on activiation
 register_activation_hook(__FILE__, 'pl_create_token_table');
 function pl_create_token_table() {
@@ -69,6 +57,66 @@ function pl_generate_user_token($user_id) {
   return $token;
 }
 
+
+//begin PHPmailer setup
+
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require './vendor/autoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+//SMTP::DEBUG_OFF = off (for production use)
+//SMTP::DEBUG_CLIENT = client messages
+//SMTP::DEBUG_SERVER = client and server messages
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//Set the hostname of the mail server
+$mail->Host = 'mail.example.com';
+//Set the SMTP port number - likely to be 25, 465 or 587
+$mail->Port = 25;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication
+$mail->Username = 'yourname@example.com';
+//Password to use for SMTP authentication
+$mail->Password = 'yourpassword';
+//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('whoto@example.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer SMTP test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+
+//SMTP XCLIENT attributes can be passed with setSMTPXclientAttribute method
+//$mail->setSMTPXclientAttribute('LOGIN', 'yourname@example.com');
+//$mail->setSMTPXclientAttribute('ADDR', '10.10.10.10');
+//$mail->setSMTPXclientAttribute('HELO', 'test.example.com');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
+
 //Send email with private link
 function pl_send_private_link_email($user_email, $user_id, $email_subject, $page_slug) {
   $token = pl_generate_user_token($user_id);
@@ -76,7 +124,7 @@ function pl_send_private_link_email($user_email, $user_id, $email_subject, $page
 
   $message = 'Here is your private link: ' . $private_link;
 
-  wp_mail($user_email, $email_subject, $message);
+  wp_mail($user_email, $email_subject, $message); //replace this with PHPMailer
 }
 
 //check user token for page access
