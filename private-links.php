a292df020839c46e99c9ad0224b096e3d508914c
Matt Jones <me@mattjones.tech>
2024-06-26 21:00:11 -0400

    move PHPMailer setup inside mail function

--- private-links.php
+++ private-links.php
@@ -6,6 +6,11 @@
  * Author: Matt Jones
  */
 
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+
 if (!defined('ABSPATH')) {
   exit; //Exit if accessed directly
 }
@@ -58,24 +63,24 @@ function pl_generate_user_token($user_id) {
 }
 
 
-//begin PHPmailer setup
+//Send email with private link
+function pl_send_private_link_email($user_email, $user_id, $email_subject, $page_slug) {
 
-//Import the PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+
+//begin PHPmailer setup
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
 date_default_timezone_set('Etc/UTC');
 
-require './vendor/autoload.php';
+require ((__DIR__) . '/vendor/autoload.php');
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer();
 //Tell PHPMailer to use SMTP
 $mail->isSMTP();
 //Enable SMTP debugging
-//SMTP::DEBUG_OFF = off (for production use)
+// SMTP::DEBUG_OFF = off (for production use)
 //SMTP::DEBUG_CLIENT = client messages
 //SMTP::DEBUG_SERVER = client and server messages
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
@@ -99,11 +104,11 @@ $mail->addAddress('whoto@example.com', 'John Doe');
 $mail->Subject = 'PHPMailer SMTP test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+// $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
 //Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
+// $mail->addAttachment('images/phpmailer_mini.png');
 
 //SMTP XCLIENT attributes can be passed with setSMTPXclientAttribute method
 //$mail->setSMTPXclientAttribute('LOGIN', 'yourname@example.com');
@@ -117,8 +122,6 @@ if (!$mail->send()) {
     echo 'Message sent!';
 }
 
-//Send email with private link
-function pl_send_private_link_email($user_email, $user_id, $email_subject, $page_slug) {
   $token = pl_generate_user_token($user_id);
   $private_link = home_url($page_slug . '?access_token=' . $token);
 
