e105ab86af1082713cf06474ed4a645dae2d3575
Matt Jones <me@mattjones.tech>
2024-06-29 16:59:47 -0400

    add smtp host field, submit button to settings form

--- includes/smtp-settings.php
+++ includes/smtp-settings.php
@@ -3,5 +3,20 @@ function pl_render_smtp_settings_page() {
 ?>
 <div class="wrap">
 <h1>Hello world from the SMTP Settings Page!</h1>
+<form>
+<table class="form-table">
+<tr>
+<th>SMTP Host</th>
+<td><input type="text" name="smtp_host" required /></td>
+</tr>
+<tr>
+<td><input type="submit" name="submit" class="button-primary" value="Save Settings"</td>
+</tr>
+</table>
+</form>
 </div>
-<?php }
+<?php 
+  if(isset($_POST['submit'])) {
+  $smtp_host = sanitize_text_field($_POST['smtp_host']);
+  }
+}
