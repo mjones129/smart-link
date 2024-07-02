11f9c1b133708a9a878a2caa386dcf89121a0c64
Matt Jones <me@mattjones.tech>
2024-07-02 06:17:51 -0400

    syntax error on register_activation_hook

--- private-links.php
+++ private-links.php
@@ -22,7 +22,7 @@ include_once plugin_dir_path(__FILE__) . '/includes/smtp-settings.php';
 include_once plugin_dir_path(__FILE__) . '/includes/send-private-link.php';
 
 //plugin setup
-register_activation_hook(__FILE__ . 'pl_plugin_activate');
+register_activation_hook(__FILE__,  'pl_plugin_activate');
 
 function pl_plugin_activate() {
   require_once plugin_dir_path(__FILE__) . 'activate.php';
