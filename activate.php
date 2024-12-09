e8e130ad7dc7a3beade8e09d874a30b2036544c6
Matt Jones <me@mattjones.tech>
2024-12-09 18:49:29 -0500

    tweak formatting
    formatting was automattically applied when saved.

--- activate.php
+++ activate.php
@@ -1,18 +1,16 @@
 <?php
 
-function sl_activate_plugin() {
+function sl_activate_plugin()
+{
 
-require_once(plugin_dir_path(__FILE__) . '/classes/slinit.class.php');
+    require_once(plugin_dir_path(__FILE__) . '/classes/slinit.class.php');
 
-$init = new SLinit();
+    $init = new SLinit();
 
-$init->sl_create_tokens_table();
+    $init->sl_create_tokens_table();
 
-$init->sl_create_smtp_table();
+    $init->sl_create_smtp_table();
 
-$init->sl_generate_encryption_key();    
+    $init->sl_generate_encryption_key();
 
 }
-
-
-
