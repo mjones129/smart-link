9d9203353e0134c9a80f00e7adacde480547804d
Matt <me@mattjones.tech>
2024-12-29 09:24:31 +0800

    add admin page links

--- activate.php
+++ activate.php
@@ -9,8 +9,21 @@ function sl_activate_plugin()
 
     $init->sl_create_tokens_table();
 
-    $init->sl_create_smtp_table();
+    // $init->sl_create_smtp_table();
 
-    $init->sl_generate_encryption_key();
+    // $init->sl_generate_encryption_key();
 
+    
+
+}
+
+
+function sl_add(){
+    add_filter('post_row_actions', array($this, 'sl_add_link_button'), 10, 2);
+    add_filter('page_row_actions', array($this, 'sl_add_link_button'), 10, 2);
+}
+
+function sl_add_link_button($actions, $post){
+    $actions['clear_cache_link'] = '<a data-id="'.$post->ID.'" data-nonce="'.wp_create_nonce('clear-cache_'.$post->ID).'" id="wpfc-clear-cache-link-'.$post->ID.'" style="cursor:pointer;">' . __('Clear Cache') . '</a>';
+    return $actions;
 }
