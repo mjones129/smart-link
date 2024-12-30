5737fc8d72ce68ac1f604180061865bea83914ae
Matt <me@mattjones.tech>
2024-12-30 21:33:54 +0800

    alert post ID on page click

---
+++ js/copyPrivateLink.js
@@ -0,0 +1,32 @@
+if(window.attachEvent) {
+    window.attachEvent('onload', sl_column_button_action);
+} else {
+    if(window.onload) {
+        var curronload_1 = window.onload;
+        var newonload_1 = function(evt) {
+            curronload_1(evt);
+            sl_column_button_action(evt);
+        };
+        window.onload = newonload_1;
+    } else {
+        window.onload = sl_column_button_action;
+    }
+}
+function sl_column_button_action(){
+        // jQuery(/<a\s+[^>]*id="sl-copy-link-\d+"/).on( "click", function(e){
+        //     let post_id = jQuery(e.target).attr("data-id");
+        //     let nonce = jQuery(e.target).attr("data-nonce");
+
+        //     alert(`post ID: ${post_id}`);
+        // });
+
+        jQuery('a[id^="sl-copy-link-"]').filter(function() {
+            return this.id.match(/^sl-copy-link-\d+$/);
+        }).on("click", function(e){
+            let post_id = jQuery(e.target).attr("data-id");
+            let nonce = jQuery(e.target).attr("data-nonce");
+        
+            alert(`post ID: ${post_id}`);
+        });
+        
+}
