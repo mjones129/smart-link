7aa19e8db748d1ea88333b380578c934a537c21a
Matt <me@mattjones.tech>
2025-01-04 21:35:52 -0500

    generate token with js instead

--- js/copyPrivateLink.js
+++ js/copyPrivateLink.js
@@ -12,23 +12,43 @@ if(window.attachEvent) {
         window.onload = sl_column_button_action;
     }
 }
+
+function generateToken() {
+    const array = new Uint8Array(8);
+    window.crypto.getRandomValues(array);
+    let token = Array.from(array, byte => ('0' + byte.toString(16)).slice(-2)).join('');
+    return token;
+}
+
 function sl_column_button_action(){
         jQuery('a[id^="sl-copy-link-"]').filter(function() {
             return this.id.match(/^sl-copy-link-\d+$/);
         }).on("click", function(e){
             let post_id = jQuery(e.target).attr("data-id");
             let nonce = jQuery(e.target).attr("data-nonce");
+
+            jQuery.ajax({
+                url: `https://mattjones.tech/wp-json/wp/v2/pages/${post_id}/`,
+                type: 'GET',
+                error: () => {alert("That didn't work. Please ensure this page is public and published before attempting to copy the secured link."); },
+                success: (response) => {
+                    console.log(`random token: ${generateToken()}`)
+                    console.log(`response slug: ${response.slug}`)
+                    console.log(`full private link: ${window.location.origin}/${response.slug}?access_token=${generateToken()}`)
+                }
+            });
         
-            alert(`post ID: ${post_id}`);
+            // alert(`post ID: ${post_id}`);
+            // alert(`tokenized link: ${tokenizedLink}`);
         });
 }
 
-button.addEventListener("click", () => writeClipboardText("<empty clipboard>"));
+// button.addEventListener("click", () => writeClipboardText("<empty clipboard>"));
 
-async function writeClipboardText(text) {
-  try {
-    await navigator.clipboard.writeText(text);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+// async function writeClipboardText(text) {
+//   try {
+//     await navigator.clipboard.writeText(text);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
