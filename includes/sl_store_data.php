f03509c680975504550179a562157715a2bc5a1a
Matt <me@mattjones.tech>
2025-01-13 21:50:11 -0500

    remove logged out hook, fix ajax action name, check for data before setting variable

--- includes/sl_store_data.php
+++ includes/sl_store_data.php
@@ -1,26 +1,26 @@
 <?php
 
-add_action( 'wp_ajax_store_sl_save_token', 'sl_save_token' );
-add_action( 'wp_ajax_nopriv_store_sl_save_token', 'sl_save_token' );
+add_action( 'wp_ajax_sl_save_token', 'sl_save_token' );
 
 function sl_save_token() {
+    global $wpdb;
 
     if( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'sl_save_token' ) ) {
         wp_send_json_error( 'Invalid nonce' );
         return;
     }
 
-    global $wpdb;
-
-    $token = sanitize_text_field( $_POST['token'] );
-    $page_id = sanitize_text_field( $_POST['page_id'] );
-    $current_time = sanitize_text_field( $_POST['current_time'] );
+    $token = isset( $_POST['token'] ) ? sanitize_text_field( $_POST['token'] ) : '';
+    $page_id = isset( $_POST['page_id'] ) ? sanitize_text_field( $_POST['page_id'] ) : '';
+    $current_time = isset( $_POST['current_time'] ) ? sanitize_text_field( $_POST['current_time'] ) : '';
 
     //Calculate expiration time (24 hours from current time)
     $expiration_time = date( 'Y-m-d H:i:s', strtotime( $current_time . ' + 24 hours' ) );
 
-    $wpdb->insert(
-        $wpdb->prefix . 'sl_tokens',
+    $tokens_table = $wpdb->prefix . 'sl_tokens';
+
+    $inserted = $wpdb->insert(
+        $tokens_table,
         array(
             'token' => $token,
             'page_ID' => $page_id,
@@ -28,6 +28,10 @@ function sl_save_token() {
         )
     );
 
+    if( ! $inserted ) {
+        wp_send_json_error( 'Error storing data: ' $wpdb->last_error );
+        return;
+    }
     wp_send_json_success( 'Data Stored!' );
 }
 
