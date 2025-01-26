b90796e872d193490f45832d05f0393f0b1b1488
Matt <me@mattjones.tech>
2025-01-26 01:07:11 -0500

    require files to be bundled with webpack

--- js/copyPrivateLink.js
+++ js/copyPrivateLink.js
@@ -1,3 +1,9 @@
+const Toastify = require('toastify-js');
+require('../node_modules/toastify-js/src/toastify.css');
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../css/pl-style.css');
+
+
 if(window.attachEvent) {
     window.attachEvent('onload', sl_column_button_action);
 } else {
