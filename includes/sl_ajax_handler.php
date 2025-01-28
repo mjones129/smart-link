7f3a9f4ace16977fc53c2eeb34cc4f76073d8061
Matt <me@mattjones.tech>
2025-01-27 21:35:29 -0500

    prepare database queries

--- includes/sl_ajax_handler.php
+++ includes/sl_ajax_handler.php
@@ -1,57 +1,64 @@
 <?php
 
-add_action( 'wp_ajax_sl_save_token', 'sl_save_token' );
+add_action('wp_ajax_sl_save_token', 'sl_save_token');
 
-function sl_save_token() {
+function sl_save_token()
+{
     global $wpdb;
 
-    $nonce = isset( $_POST['nonce'] ) ? sanitize_text_field( wp_unslash($_POST['nonce']) ) : '';
-    $page_id = isset( $_POST['page_id'] ) ? sanitize_text_field( wp_unslash($_POST['page_id']) ) : '';
+    $nonce = isset($_POST['nonce']) ? sanitize_text_field(wp_unslash($_POST['nonce'])) : '';
+    $page_id = isset($_POST['page_id']) ? sanitize_text_field(wp_unslash($_POST['page_id'])) : '';
 
-    if( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( wp_unslash($nonce), 'sl-copy-link_' . $page_id ) ) {
-        wp_send_json_error( 'Invalid nonce' );
+    if (! isset($_POST['nonce']) || ! wp_verify_nonce(wp_unslash($nonce), 'sl-copy-link_' . $page_id)) {
+        wp_send_json_error('Invalid nonce');
         return;
     }
 
-    $token = isset( $_POST['token'] ) ? sanitize_text_field( $_POST['token'] ) : '';
-    
-    $slug = isset( $_POST['slug'] ) ? sanitize_text_field( $_POST['slug'] ) : '';
-    $current_time = isset( $_POST['current_time'] ) ? sanitize_text_field( $_POST['current_time'] ) : '';
-    
+    $token = isset($_POST['token']) ? sanitize_text_field(wp_unslash($_POST['token'])) : '';
+
+    $slug = isset($_POST['slug']) ? sanitize_text_field(wp_unslash($_POST['slug'])) : '';
+    $current_time = isset($_POST['current_time']) ? sanitize_text_field(wp_unslash($_POST['current_time'])) : '';
+
 
     //Calculate expiration time (24 hours from current time)
-    $expiration_time = gmdate( 'Y-m-d H:i:s', strtotime( $current_time . ' + 24 hours' ) );
+    $expiration_time = gmdate('Y-m-d H:i:s', strtotime($current_time . ' + 24 hours'));
 
     $tokens_table = $wpdb->prefix . 'sl_tokens';
 
-    $inserted = $wpdb->insert(
-        $tokens_table,
-        array(
-            'token' => $token,
-            'page_ID' => $page_id,
-            'slug' => $slug,
-            'expiration' => $expiration_time,
+
+    $inserted = $wpdb->query(
+        $wpdb->prepare(
+            "INSERT INTO %i (page_ID, token, slug, expiration) VALUES (%d, %s, %s, %s)",
+            $tokens_table,
+            $page_id,
+            $token,
+            $slug,
+            $expiration_time
         )
     );
-
-    if( ! $inserted ) {
-        wp_send_json_error( 'Error storing data: ' . $wpdb->last_error );
-        return;
+    
+    if ($inserted) {
+        wp_send_json_success('Data inserted successfully.');
+    } else {
+        wp_send_json_error('Error inserting data.');
     }
-    wp_send_json_success( 'Data Stored!' );
 }
 
-add_action( 'wp_ajax_sl_delete_token', 'sl_delete_token' );
+add_action('wp_ajax_sl_delete_token', 'sl_delete_token');
 
-function sl_delete_token() {
+function sl_delete_token()
+{
     global $wpdb;
 
-    // if( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'sl-delete-link_' . $_POST['token_id'] ) ) {
-    //     wp_send_json_error( 'Invalid nonce' );
-    //     return;
-    // }
+    $nonce = isset($_POST['nonce']) ? sanitize_text_field(wp_unslash($_POST['nonce'])) : '';
+    $token_id = isset($_POST['token_id']) ? sanitize_text_field(wp_unslash($_POST['token_id'])) : '';
 
-    $id = isset( $_POST['token_id'] ) ? sanitize_text_field( $_POST['token_id'] ) : '';
+    if (! isset($_POST['nonce']) || ! wp_verify_nonce($nonce, 'sl-delete-link_' . $token_id)) {
+        wp_send_json_error('Invalid nonce');
+        return;
+    }
+
+    $id = isset($_POST['token_id']) ? sanitize_text_field(wp_unslash($_POST['token_id'])) : '';
 
     $tokens_table = $wpdb->prefix . 'sl_tokens';
 
@@ -62,9 +69,9 @@ function sl_delete_token() {
         )
     );
 
-    if( ! $deleted ) {
-        wp_send_json_error( 'Error deleting data: ' . $wpdb->last_error );
+    if (! $deleted) {
+        wp_send_json_error('Error deleting data: ' . $wpdb->last_error);
         return;
     }
-    wp_send_json_success( 'Data Deleted!' );
+    wp_send_json_success('Data Deleted!');
 }
