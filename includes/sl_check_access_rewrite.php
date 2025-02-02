07e5d8d3d4846c02030c959e1b40a56f832a5123
Matt <me@mattjones.tech>
2025-02-01 22:13:18 -0500

    troubleshooting ajax responses

--- includes/sl_check_access_rewrite.php
+++ includes/sl_check_access_rewrite.php
@@ -10,7 +10,9 @@ function sl_check_token()
         return;
     }
 
-    sl_get_links();
+    // sl_token_checker();
+    wp_send_json_success('please be something different than a zero');
+
 }
 add_action('wp_ajax_nopriv_sl_check_token', 'sl_check_token');
 
@@ -21,18 +23,79 @@ function sl_get_links() {
     $link_query = $wpdb->prepare("SELECT * FROM %i;", $escaped_table);
     $sl_links = $wpdb->get_results($link_query, ARRAY_A);
     if (!empty($sl_links)) {
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Page ID</th><th>Slug</th><th>Token</th><th>Expiration</th><th>Used</th></tr>";
 
-        foreach($sl_links as $link) {
-            echo "<tr>";
-            echo "<td>" . esc_html($link['id']) . "</td>";
-            echo "<td>" . esc_html($link['page_ID']) . "</td>";
-            echo "<td>" . esc_html($link['slug']) . "</td>";
-            echo "<td>" . esc_html($link['token']) . "</td>";
-            echo "<td>" . esc_html($link['expiration']) . "</td>";
-            echo "<td>" . esc_html($link['used']) . "</td>";
+        // echo "<table>";
+        // echo "<tr><th>ID</th><th>Page ID</th><th>Slug</th><th>Token</th><th>Expiration</th><th>Used</th></tr>";
+
+        // foreach($sl_links as $link) {
+        //     echo "<tr>";
+        //     echo "<td>" . esc_html($link['id']) . "</td>";
+        //     echo "<td>" . esc_html($link['page_ID']) . "</td>";
+        //     echo "<td>" . esc_html($link['slug']) . "</td>";
+        //     echo "<td>" . esc_html($link['token']) . "</td>";
+        //     echo "<td>" . esc_html($link['expiration']) . "</td>";
+        //     echo "<td>" . esc_html($link['used']) . "</td>";
+        // }
+        // echo "</table>";
+        return $sl_links;
+    }
+}
+
+$sl_links = sl_get_links();
+
+$sl_token_validation = array();
+
+$match_found = false;
+
+function sl_is_slug_protected() {
+    $current_slug = isset($_POST['slug']) ? sanitize_text_field(wp_unslash($_POST['slug'])) : '';
+    global $sl_links, $sl_token_validation, $match_found;
+    $total_links = count($sl_links);
+    for ($i = 0; $i < $total_links; $i++) {
+        if($current_slug === $sl_links[$i]['slug']) {
+            $match_found = true;
+            $sl_token_validation['slug'][$current_slug];
+            break;
         }
-        echo "</table>";
+    }
+
+}
+
+$i = 0;
+$index_match = null;
+
+function sl_is_token_valid() {
+    global $sl_links, $index_match, $sl_token_validation, $match_found;
+    $user_token = isset($_POST['access_token']) ? sanitize_text_field(wp_unslash($_GET['access_token'])) : '';
+    if($match_found) {
+        for ($i = 0; $i <= count($sl_links); $i++) {
+            if($user_token === $sl_links[$i]['token']) {
+                $index_match = $i;
+                $sl_token_validation['token'][$sl_links[$i]['token']];
+                break;
+            }
+        }
+    }
+    wp_send_json_error('Invalid access token.');
+}
+
+function sl_expiration_still_valid() {
+    global $sl_links, $index_match, $sl_token_validation, $match_found;
+    $current_time = current_time('mysql');
+    if($match_found) {
+        if($current_time <= $sl_links[$index_match]['expiration']) {
+            $sl_token_validation['expiration'][0];
+            return $index_match;
+        }        
+    }
+    wp_send_json_error('Access token expired.');
+}
+
+function sl_token_checker() {
+    global $sl_token_validation, $sl_links, $index_match, $match_found;
+    if($match_found) {
+        if($sl_links[$index_match]['slug'] === $sl_token_validation['slug'] && $sl_links[$index_match]['token'] === $sl_token_validation['token'] && $sl_token_validation['expiration'] === 0) {
+            wp_send_json_success('Token accepted.');
+        }
     }
 }
--- js/sendNonce.js
+++ js/sendNonce.js
@@ -6,6 +6,8 @@ jQuery(document).ready(function($) {
     function sendNonceViaHeader() {
         const tokenCheck = window.location.search.split('access_token=')[1];
         const token = tokenCheck === undefined ? '' : tokenCheck;
+        const slug = window.location.pathname;
+        const cleanSlug = slug.replace(/^\/|\/$/g, '');
         console.log(`nonce: ${sl_ajax_object.nonce}`); 
         $.ajax({
             url: sl_ajax_object.ajax_url,
@@ -16,17 +18,27 @@ jQuery(document).ready(function($) {
             data: {
                 action: 'sl_check_token',
                 token: token,
+                slug: cleanSlug,
                 nonce: sl_ajax_object.nonce,
                 page_id: 123,
                 current_time: new Date().toISOString()
             },
             success: function(response) {
-                output = JSON.stringify(response);
-                $(".page").append(output);
-                console.log(`success: ${output}`);
+                if(response) {
+                    output = JSON.stringify(response)
+                    console.log(`ajax response success: ${output}`);
+                } else {
+                    let currentURL = window.location.href;
+                    if(!currentURL.includes('redirected=true')) {
+                        window.location.href = '/access-denied/?redirected=true';
+                        output = JSON.stringify(response);
+                        console.log(`Response object: ${output}`);
+                    }
+                }
             },
             error: function(response) {
-                output = JSON.stringify(response)
+                output = JSON.stringify(response);
+                $(".page").append(`error: ${output}`);
                 console.log(`Error: ${output}`);
             }
         });
